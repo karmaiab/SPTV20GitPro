33ab7d937ad48d6ddb2f680c0c0ff350d657ad14
sergeyts <sergeysill@gmail.com>
2021-09-16 13:05:15 +0300

    first version of the project

---
+++ src/sptv20gitpro/SPTV20GitPro.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sptv20gitpro;
+
+/**
+ *
+ * @author User
+ */
+public class SPTV20GitPro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("First version of the project");
+    }
+    
+}
